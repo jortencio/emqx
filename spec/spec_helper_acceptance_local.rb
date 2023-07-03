d5e08bf3d3ab78bd734a3029bf88034d5286d749
Jason Ortencio <jason.ortencio@puppet.com>
2023-07-04 09:35:11 +1000

    Add class Litmus Helper

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,3 +1,10 @@
+require 'singleton'
+
+class LitmusHelper
+  include Singleton
+  include PuppetLitmus
+end
+
 RSpec.configure do |c|
   c.before :suite do
     LitmusHelper.instance.apply_manifest(File.read(File.join(__dir__, 'setup_acceptance_node.pp')))
