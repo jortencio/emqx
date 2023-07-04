02c37c4dd36b658533d350bb713f233eb87e3955
Jason Ortencio <jason.ortencio@puppet.com>
2023-07-04 13:01:46 +1000

    Clean up syntax

--- spec/acceptance/emqx_spec.rb
+++ spec/acceptance/emqx_spec.rb
@@ -1,27 +1,28 @@
 require 'spec_helper_acceptance'
 
 describe 'emqx' do
-  context 'default parameters'
-  let(:pp) do
-    <<-MANIFEST
+  context 'default parameters' do
+    let(:pp) do
+      <<-MANIFEST
       class { 'emqx': }
-    MANIFEST
-  end
+      MANIFEST
+    end
 
-  it 'applies idempotently' do
-    idempotent_apply(pp)
-  end
+    it 'applies idempotently' do
+      idempotent_apply(pp)
+    end
 
-  describe package('emqx') do
-    it { is_expected.to be_installed }
-  end
+    describe package('emqx') do
+      it { is_expected.to be_installed }
+    end
 
-  describe service('emqx') do
-    it { is_expected.to be_enabled }
-    it { is_expected.to be_running }
-  end
+    describe service('emqx') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
 
-  describe port(1883) do
-    it { is_expected.to be_listening }
+    describe port(1883) do
+      it { is_expected.to be_listening }
+    end
   end
 end
